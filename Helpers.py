d1fe1c107657045749aff30d7556b470b295df9f
LukeDitria <luke.ditria@monash.edu>
2023-09-25 12:27:49 +1000

    initial push

---
+++ Helpers.py
@@ -0,0 +1,46 @@
+import torch
+import copy
+from tqdm import trange
+
+
+def noise_from_x0(curr_img, img_pred, alpha):
+    return (curr_img - alpha.sqrt() * img_pred)/((1 - alpha).sqrt() + 1e-4)
+
+
+def cosine_alphas_bar(timesteps, s=0.008):
+    steps = timesteps + 1
+    x = torch.linspace(0, steps, steps)
+    alphas_bar = torch.cos(((x / steps) + s) / (1 + s) * torch.pi * 0.5) ** 2
+    alphas_bar = alphas_bar / alphas_bar[0]
+    return alphas_bar
+
+
+def cold_diffuse(diffusion_model, batch_size, total_steps, device, image_size=32,
+                 no_p_bar=True, noise_sigma=1, class_indx=None):
+
+    diffusion_model.eval()
+    random_image_sample = noise_sigma * torch.randn(batch_size, 3, image_size, image_size, device=device)
+    sample_in = copy.deepcopy(random_image_sample)
+
+    alphas = torch.flip(cosine_alphas_bar(total_steps), (0,)).to(device)
+
+    for i in trange(total_steps - 1, disable=no_p_bar):
+        index = i * torch.ones(batch_size, device=device)
+
+        if class_indx is not None:
+            img_output = diffusion_model(sample_in, index, cond_input=class_indx)
+        else:
+            img_output = diffusion_model(sample_in, index)
+
+        noise = noise_from_x0(sample_in, img_output, alphas[i])
+        x0 = img_output
+
+        rep1 = alphas[i].sqrt() * x0 + (1 - alphas[i]).sqrt() * noise
+        rep2 = alphas[i + 1].sqrt() * x0 + (1 - alphas[i + 1]).sqrt() * noise
+
+        sample_in += rep2 - rep1
+
+    index = (total_steps - 1) * torch.ones(batch_size, device=device)
+    img_output = diffusion_model(sample_in, index)
+
+    return torch.clamp(img_output, -1, 1), torch.clamp(random_image_sample, -1, 1)
--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# Pytorch_Diffusion
-Simple General Purpose Pytorch Implementation of Cold Diffusion
+
+# Cold Diffusion
+A simple general purpose Pytorch implementation of Denoising Cold Diffusion
+
+# Training Examples
+<br>
+<b> Basic training command: </b><br>
+Works well for 64x64 sized images with a low-to-average diversity dataset
+
+```
+python train.py -mn test_run --dataset_root #path to dataset root#
+```
+
+<br>
+<b> Starting from an existing checkpoint: </b><br>
+The code will attempt to load a checkpoint with the name provided in the "save_dir" specified.
+
+```
+python train.py -mn test_run --load_checkpoint --dataset_root #path to dataset root#
+```
+
+<br>
+<b> Define a Custom Architecture: </b><br>
+Example showing how to define each of the main parameters of the Unet Architecture.<br>
+Increasing the model depth can help with larger images, increasing width can help with more diverse datasets
+
+```
+python train.py -mn test_run --block_widths 1 2 4 8 --ch_multi 64 --dataset_root #path to dataset root#
+```
+
+<br>
+<b> Change number of diffusion steps: </b><br>
+
+```
+python train.py -mn test_run --num_steps 200 --dataset_root #path to dataset root#
+```
+
+
+<br>
